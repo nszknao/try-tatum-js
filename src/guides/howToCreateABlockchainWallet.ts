f40c849980c23397a82522be4cd47b7b2026a171
Naoto Nishizaka <nszk.nao@gmail.com>
2021-10-04 13:57:26 +0900

    remove key

--- src/guides/howToCreateABlockchainWallet.ts
+++ src/guides/howToCreateABlockchainWallet.ts
@@ -15,17 +15,12 @@ const main = async () => {
   console.log(wallet);
 
   // Generating an address from the wallet
-  // const address = generateAddressFromXPub(
-  //   Currency.BTC,
-  //   true,
-  //   "tpubDFXzrccHFPY1HxAxj5e1nd9M86bMYs6xYUop9uuSz33siQHNJZW7cT5wfNL2tbbmofRiQSmttQBUuyRHbYeA4rwBwGB9y5eEuuKjRmKihDd",
-  //   1
-  // );
+  // const address = generateAddressFromXPub(Currency.BTC, true, wallet.xpub, 1);
 
   // const privateKey = await generatePrivateKeyFromMnemonic(
   //   Currency.BTC,
   //   true,
-  //   "absent enlist fossil program replace minor snack domain very knee ripple gather purchase grace panther empty brisk daughter later indoor april useful rack erupt",
+  //   wallet.mnemonic,
   //   1
   // );
 
@@ -36,8 +31,7 @@ const main = async () => {
   //   to: "0x687422eEA2cB73B5d3e242bA5456b782919AFc85",
   //   currency: Currency.ETH,
   //   amount: "0.01",
-  //   fromPrivateKey:
-  //     "0xcd2fe348ecbde2a9b1caf0429dfaac4b656b9d969eca290cc106e6cbb38ef1e9",
+  //   fromPrivateKey: privateKey,
   // });
   // console.log(txHash);
 };
